e2f10357dfcec9dc4649a62a43d31faa42bcc3e3
Sam Marley-Jarrett <samstah@gmail.com>
2019-09-22 00:02:20 +0800

    PEP8 fixes

--- src/main.py
+++ src/main.py
@@ -1,7 +1,4 @@
-import hashlib
-import math
 import sys
-import os
 import urllib.parse
 import base64
 from io import BytesIO
@@ -13,46 +10,49 @@ FONT = ImageFont.truetype('./HelveticaNeue-Thin.ttf', 28)
 HEIGHT = 369
 PADDING = 30
 
+
 def wrap_text(text, width, font):
     text_lines = []
     text_line = []
     text = text.replace('\n', ' [br] ')
     words = text.split()
-    font_size = font.getsize(text)
 
     for word in words:
         if word == '[br]':
             text_lines.append(' '.join(text_line))
             text_line = []
             continue
+
         text_line.append(word)
-        w, h = font.getsize(' '.join(text_line))
-        if w > width:
+        text_width, _ = font.getsize(' '.join(text_line))
+        if text_width > width:
             text_line.pop()
             text_lines.append(' '.join(text_line))
             text_line = [word]
 
-    if len(text_line) > 0:
+    if text_line:
         text_lines.append(' '.join(text_line))
 
     return text_lines
 
+
 def get_text_height(text):
     image = Image.new('RGB', (480, 500))
-    d = ImageDraw.Draw(image)
+    drawer = ImageDraw.Draw(image)
 
-    width, height = d.multiline_textsize(text, font=FONT, spacing=10)
+    _, height = drawer.multiline_textsize(text, font=FONT, spacing=10)
 
     return height
 
+
 def resize_canvas(image_path="mocking-spongebob.jpg", canvas_height=369):
-    im = Image.open(image_path)
-    width, height = im.size
+    image = Image.open(image_path)
+    width, height = image.size
 
     # Center the image
-    y = canvas_height - height
+    y_position = canvas_height - height
 
-    mode = im.mode
+    mode = image.mode
     if len(mode) == 1:  # L, 1
         new_background = (255)
     if len(mode) == 3:  # RGB
@@ -60,45 +60,47 @@ def resize_canvas(image_path="mocking-spongebob.jpg", canvas_height=369):
     if len(mode) == 4:  # RGBA, CMYK
         new_background = (255, 255, 255, 255)
 
-    newImage = Image.new(mode, (width, canvas_height), new_background)
-    newImage.paste(im, (0, y))
-    
-    return newImage
+    new_image = Image.new(mode, (width, canvas_height), new_background)
+    new_image.paste(image, (0, y_position))
+
+    return new_image
+
 
 def generate_image(text):
     text = "\n".join(wrap_text(mocking_case(text), 460, FONT))
     height = get_text_height(text)
 
     image = resize_canvas(canvas_height=(HEIGHT + PADDING + height))
-    d = ImageDraw.Draw(image)
-    d.multiline_text((10,10), text, font=FONT, fill=(0, 0, 0), spacing=10)
+    drawer = ImageDraw.Draw(image)
+    drawer.multiline_text((10, 10), text, font=FONT, fill=(0, 0, 0), spacing=10)
 
     return image
+
 
 def handle_lambda(event, context):
     text = urllib.parse.unquote_plus(event['pathParameters']['string'])
     image = generate_image(text)
-    filename = hashlib.sha1(text.encode()).hexdigest() + '.jpg'
 
-    outputFile = BytesIO()
-    image.save(outputFile, 'JPEG')
-    outputFile.seek(0)
+    output_file = BytesIO()
+    image.save(output_file, 'JPEG')
+    output_file.seek(0)
 
     response = {
         "statusCode": 200,
-        "headers": { "content-type": "image/jpeg"},
-        "body": base64.b64encode(outputFile.read()).decode('utf-8'),
+        "headers": {"content-type": "image/jpeg"},
+        "body": base64.b64encode(output_file.read()).decode('utf-8'),
         "isBase64Encoded": True,
     }
 
     return response
 
+
 def demo(text):
     text = urllib.parse.unquote_plus(text)
     image = generate_image(text)
-    filename = hashlib.sha1(text.encode()).hexdigest() + '.jpg'
 
     image.save('output.jpg', 'JPEG')
+
 
 if __name__ == '__main__':
     demo(sys.argv[1])
