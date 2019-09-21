79e8a6b44851404f7f9012d2601261feaa5d351b
Sam Marley-Jarrett <samstah@gmail.com>
2019-09-22 00:10:22 +0800

    Add a /slash command

---
+++ src/slash.py
@@ -0,0 +1,43 @@
+import urllib.parse
+import base64
+import json
+from text_manipulations import mocking_case
+
+
+def generate_slack_response(text):
+    url = 'https://mock.sam.wtf/' + urllib.parse.quote_plus(text)
+
+    body = {
+        "response_type": "in_channel",
+        "blocks": [
+            {
+                "type": "image",
+                "title": {
+                    "type": "plain_text",
+                    "text": mocking_case(text)
+                },
+                "image_url": url,
+                "alt_text": mocking_case(text)
+            }
+        ]
+    }
+
+    response = {
+        "statusCode": 200,
+        "headers": {"content-type": "application/json"},
+        "body": json.dumps(body)
+    }
+
+    return response
+
+
+def handle_lambda(event, context):
+    body = urllib.parse.parse_qs(base64.b64decode(event["body"]).decode())
+
+    text = body['text'][0]
+
+    return generate_slack_response(text)
+
+
+if __name__ == '__main__':
+    print(generate_slack_response("hello"))
